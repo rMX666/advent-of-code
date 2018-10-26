61db6788f8680b33c5e906b022dc3bb4b6be6248
Mike Stanin <mike@rm-x.tk>
2018-10-26 18:06:39 +0300

    Advent of Code 2015 - Day 03

---
+++ AoC2015/Task03/uTask_2015_03.pas
@@ -0,0 +1,97 @@
+unit uTask_2015_03;
+
+interface
+
+uses
+  uTask, System.Types, System.Generics.Collections;
+
+type
+  TTask_AoC = class (TTask)
+  private
+    FDirections: TDictionary<Char,TPoint>;
+    procedure Solve(const Part: Integer);
+  protected
+    procedure DoRun; override;
+  public
+    constructor Create; override;
+    destructor Destroy; override;
+  end;
+
+implementation
+
+uses
+  System.SysUtils;
+
+var
+  GTask: TTask_AoC;
+
+{ TTask_AoC }
+
+constructor TTask_AoC.Create;
+begin
+  inherited;
+
+  // Fill directions
+  FDirections := TDictionary<Char,TPoint>.Create(4);
+  with FDirections do
+    begin
+      Add('^', TPoint.Create(0, -1));
+      Add('v', TPoint.Create(0, 1));
+      Add('<', TPoint.Create(-1, 0));
+      Add('>', TPoint.Create(1, 0));
+    end;
+end;
+
+destructor TTask_AoC.Destroy;
+begin
+  FDirections.Free;
+  inherited;
+end;
+
+procedure TTask_AoC.DoRun;
+begin
+  Solve(1);
+  Solve(2);
+end;
+
+procedure TTask_AoC.Solve(const Part: Integer);
+var
+  I: Integer;
+  Point: TPoint;
+  Points: Array of TPoint;
+  Houses: TDictionary<TPoint,Integer>;
+begin
+  with Input do
+    try
+      Houses := TDictionary<TPoint,Integer>.Create;
+      Houses.Add(TPoint.Zero, 2);
+
+      SetLength(Points, Part);
+      for I := 0 to Part - 1 do
+        Points[I] := TPoint.Zero;
+
+      for I := 1 to Text.Trim.Length do
+        begin
+          Point := Points[I mod Part] + FDirections[Text[I]];
+          if Houses.ContainsKey(Point) then
+            Houses[Point] := Houses[Point] + 1
+          else
+            Houses.Add(Point, 1);
+
+          Points[I mod Part] := Point;
+        end;
+
+      OK(Format('Part %d: %d', [ Part, Houses.Count ]));
+    finally
+      Houses.Free;
+      Free;
+    end;
+end;
+
+initialization
+  GTask := TTask_AoC.Create(2015, 3, 'Perfectly Spherical Houses in a Vacuum');
+
+finalization
+  GTask.Free;
+
+end.
--- AoCLauncher.dpr
+++ AoCLauncher.dpr
@@ -5,7 +5,8 @@ uses
   uTask in 'uTask.pas',
   uMain in 'uMain.pas' {fMain},
   uTask_2015_01 in 'AoC2015\Task01\uTask_2015_01.pas',
-  uTask_2015_02 in 'AoC2015\Task02\uTask_2015_02.pas';
+  uTask_2015_02 in 'AoC2015\Task02\uTask_2015_02.pas',
+  uTask_2015_03 in 'AoC2015\Task03\uTask_2015_03.pas';
 
 {$R *.res}
 
