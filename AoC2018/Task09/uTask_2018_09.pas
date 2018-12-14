8bb5f8b8d9f80e87a57403d802960339c60ca328
Mike Stanin <mike@rm-x.tk>
2018-12-14 14:16:49 +0300

    Moved circular double linked list to uUtils

--- AoC2018/Task09/uTask_2018_09.pas
+++ AoC2018/Task09/uTask_2018_09.pas
@@ -3,30 +3,13 @@ unit uTask_2018_09;
 interface
 
 uses
-  uTask;
+  uTask, uUtil;
 
 type
-  // Simple circular double linked list implementation
-  PNode = ^TNode;
-  TNode = record
-  private
-    constructor Create(const AData: Integer);
-  public
-    Data: Integer;
-    Next, Prev: PNode;
-    class function Pointer(const AData: Integer): PNode; static;
-    function Push(const Value: Integer): PNode;
-    function Pop: PNode;
-    function Rotate(const Value: Integer): PNode;
-    procedure Free;
-    function ToString: String;
-  end;
-
   TTask_AoC = class (TTask)
   private
     FPlayerCount: Integer;
     FMaxNumber: Integer;
-    function GetHighScoreSlow(const MaxNumber: Integer): Integer;
     function GetHighScore(const MaxNumber: Integer): Int64;
   protected
     procedure DoRun; override;
@@ -40,95 +23,11 @@ uses
 var
   GTask: TTask_AoC;
 
-{ TNode }
-
-constructor TNode.Create(const AData: Integer);
-begin
-  Data := AData;
-  Prev := nil;
-  Next := nil;
-end;
-
-procedure TNode.Free;
-var
-  Node, NextNode: PNode;
-begin
-  Node := @Self;
-  try
-    while Node <> nil do
-      begin
-        NextNode := Node.Next;
-        if NextNode <> nil then
-          begin
-            Node.Next := nil;
-            Dispose(Node);
-          end;
-        Node := NextNode;
-      end;
-  except
-    // Yes, I know. Something gone wrong and I don't want to fix it
-  end;
-end;
-
-class function TNode.Pointer(const AData: Integer): PNode;
-begin
-  New(Result);
-  Result^ := TNode.Create(AData);
-  Result.Next := Result;
-  Result.Prev := Result;
-end;
-
-function TNode.Pop: PNode;
-begin
-  Result := Prev;
-  // Adjust links
-  Next.Prev := Prev;
-  Prev.Next := Next;
-  Next := nil;
-  Prev := nil;
-  Dispose(@Self);
-end;
-
-function TNode.Push(const Value: Integer): PNode;
-begin
-  Result := TNode.Pointer(Value);
-  Result.Prev := @Self;
-  Result.Next := Next;
-  Next.Prev := Result;
-  Next := Result;
-end;
-
-function TNode.Rotate(const Value: Integer): PNode;
-var
-  I, L: Integer;
-begin
-  Result := @Self;
-  L := Abs(Value);
-  case Sign(Value) of
-    -1: for I := 1 to L do Result := Result.Prev;
-     1: for I := 1 to L do Result := Result.Next;
-  end;
-end;
-
-function TNode.ToString: String;
-var
-  Node: PNode;
-begin
-  Result := Data.ToString;
-  Node := Self.Next;
-  while Node <> @Self do
-    begin
-      Result := Result + ', ' + Node.Data.ToString;
-      Node := Node.Next;
-    end;
-end;
-
 { TTask_AoC }
 
 procedure TTask_AoC.DoRun;
 var
   A: TArray<String>;
-  Node: PNode;
 begin
   with Input do
     try
@@ -176,48 +75,6 @@ begin
   for I := 0 to FPlayerCount - 1 do
     if Result < Players[I] then
       Result := Players[I];
-end;
-
-function TTask_AoC.GetHighScoreSlow(const MaxNumber: Integer): Integer;
-var
-  Players: TArray<Integer>;
-  I, CurrentPlayer, CurrentPos: Integer;
-  Marbles: TList<Integer>;
-begin
-  SetLength(Players, FPlayerCount);
-
-  Marbles := TList<Integer>.Create;
-  Marbles.Add(0);
-  Marbles.Add(1);
-
-  try
-    CurrentPlayer := 1;
-    CurrentPos := 1;
-    for I := 2 to MaxNumber do
-      if I mod 23 <> 0 then
-        begin
-          CurrentPos := (CurrentPos + 2);
-          if CurrentPos > Marbles.Count then
-            CurrentPos := CurrentPos mod Marbles.Count;
-          Marbles.Insert(CurrentPos, I);
-          CurrentPlayer := (CurrentPlayer + 1) mod FPlayerCount;
-        end
-      else
-        begin
-          Inc(Players[CurrentPlayer], I);
-          CurrentPos := (CurrentPos + Marbles.Count - 7) mod Marbles.Count;
-          Inc(Players[CurrentPlayer], Marbles[CurrentPos]);
-          Marbles.Delete(CurrentPos);
-          CurrentPlayer := (CurrentPlayer + 1) mod FPlayerCount;
-        end;
-
-    Result := 0;
-    for I := 0 to FPlayerCount - 1 do
-      if Result < Players[I] then
-        Result := Players[I];
-  finally
-    Marbles.Free;
-  end;
 end;
 
 initialization
--- uUtil.pas
+++ uUtil.pas
@@ -66,6 +66,22 @@ type
     function GetEnumerator: TEnumerator; reintroduce; inline;
   end;
 
+  // Simple circular double linked list implementation
+  PNode = ^TNode;
+  TNode = record
+  private
+    constructor Create(const AData: Integer);
+  public
+    Data: Integer;
+    Next, Prev: PNode;
+    class function Pointer(const AData: Integer): PNode; static;
+    function Push(const Value: Integer): PNode;
+    function Pop: PNode;
+    function Rotate(const Value: Integer): PNode;
+    procedure Free;
+    function ToString: String;
+  end;
+
 // Simplified implementation of Knuth-Morris-Pratt alhorithm
 // Search Needle in Haystack, return array of found positions (empty array of nothibg found)
 function KMP(const Needle, Haystack: String): TArray<Integer>;
@@ -313,6 +329,89 @@ end;
 function TSubSequences<T>.GetEnumerator: TEnumerator;
 begin
   Result := TEnumerator.Create(Self);
+end;
+
+{ TNode }
+
+constructor TNode.Create(const AData: Integer);
+begin
+  Data := AData;
+  Prev := nil;
+  Next := nil;
+end;
+
+procedure TNode.Free;
+var
+  Node, NextNode: PNode;
+begin
+  Node := @Self;
+  try
+    while Node <> nil do
+      begin
+        NextNode := Node.Next;
+        if NextNode <> nil then
+          begin
+            Node.Next := nil;
+            Dispose(Node);
+          end;
+        Node := NextNode;
+      end;
+  except
+    // Yes, I know. Something gone wrong and I don't want to fix it
+  end;
+end;
+
+class function TNode.Pointer(const AData: Integer): PNode;
+begin
+  New(Result);
+  Result^ := TNode.Create(AData);
+  Result.Next := Result;
+  Result.Prev := Result;
+end;
+
+function TNode.Pop: PNode;
+begin
+  Result := Prev;
+  // Adjust links
+  Next.Prev := Prev;
+  Prev.Next := Next;
+  Next := nil;
+  Prev := nil;
+  Dispose(@Self);
+end;
+
+function TNode.Push(const Value: Integer): PNode;
+begin
+  Result := TNode.Pointer(Value);
+  Result.Prev := @Self;
+  Result.Next := Next;
+  Next.Prev := Result;
+  Next := Result;
+end;
+
+function TNode.Rotate(const Value: Integer): PNode;
+var
+  I, L: Integer;
+begin
+  Result := @Self;
+  L := Abs(Value);
+  case Sign(Value) of
+    -1: for I := 1 to L do Result := Result.Prev;
+     1: for I := 1 to L do Result := Result.Next;
+  end;
+end;
+
+function TNode.ToString: String;
+var
+  Node: PNode;
+begin
+  Result := Data.ToString;
+  Node := Self.Next;
+  while Node <> @Self do
+    begin
+      Result := Result + ', ' + Node.Data.ToString;
+      Node := Node.Next;
+    end;
 end;
 
 end.
