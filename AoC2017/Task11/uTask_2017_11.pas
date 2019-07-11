bf80575f1dd0fdec218ed26f64b0519354d0a333
Mike Stanin <mike@rm-x.tk>
2019-07-11 18:02:28 +0300

    Advent of Code 2017 - Day 11

---
+++ AoC2017/Task11/uTask_2017_11.pas
@@ -0,0 +1,97 @@
+unit uTask_2017_11;
+
+interface
+
+uses
+  System.Generics.Collections, System.Types, uTask;
+
+type
+  TDirections = TList<TPoint>;
+
+  TTask_AoC = class (TTask)
+  private
+    FDirections: TDirections;
+    procedure LoadDirections;
+    function GetDistance(const P: TPoint): Integer;
+    procedure FindDistances(out EndDist, FurtherDist: Integer);
+  protected
+    procedure DoRun; override;
+  end;
+
+implementation
+
+uses
+  System.SysUtils, System.Math;
+
+var
+  GTask: TTask_AoC;
+
+{ TTask_AoC }
+
+procedure TTask_AoC.DoRun;
+var
+  Part1, Part2: Integer;
+begin
+  FDirections := TDirections.Create;
+  try
+    LoadDirections;
+    FindDistances(Part1, Part2);
+    OK(Format('Part 1: %d, Part 2: %d', [ Part1, Part2 ]));
+  finally
+    FDirections.Free;
+  end;
+end;
+
+procedure TTask_AoC.FindDistances(out EndDist, FurtherDist: Integer);
+var
+  I: Integer;
+  P: TPoint;
+begin
+  P := TPoint.Zero;
+  FurtherDist := 0;
+  for I := 0 to FDirections.Count - 1 do
+    begin
+      P := P + FDirections[I];
+      EndDist := GetDistance(P);
+      if FurtherDist < EndDist then
+        FurtherDist := EndDist;
+    end;
+end;
+
+function TTask_AoC.GetDistance(const P: TPoint): Integer;
+begin
+  Result := Abs(Abs(P.X) - Abs(P.Y)) div 2 + Abs(P.Y);
+end;
+
+procedure TTask_AoC.LoadDirections;
+var
+  I: Integer;
+  A: TArray<String>;
+  Dir: TDictionary<String,TPoint>;
+begin
+  Dir := TDictionary<String,TPoint>.Create;
+  Dir.Add('n',  TPoint.Create( 2,  0));
+  Dir.Add('nw', TPoint.Create( 1, -1));
+  Dir.Add('ne', TPoint.Create( 1,  1));
+  Dir.Add('s',  TPoint.Create(-2,  0));
+  Dir.Add('sw', TPoint.Create(-1, -1));
+  Dir.Add('se', TPoint.Create(-1,  1));
+
+  with Input do
+    try
+      A := Text.Trim.Split([',']);
+      for I := 0 to Length(A) - 1 do
+        FDirections.Add(Dir[A[I]]);
+    finally
+      Free;
+      Dir.Free;
+    end;
+end;
+
+initialization
+  GTask := TTask_AoC.Create(2017, 11, 'Hex Ed');
+
+finalization
+  GTask.Free;
+
+end.
--- AoCLauncher.dpr
+++ AoCLauncher.dpr
@@ -78,6 +78,7 @@ uses
   uTask_2017_08 in 'AoC2017\Task08\uTask_2017_08.pas',
   uTask_2017_09 in 'AoC2017\Task09\uTask_2017_09.pas',
   uTask_2017_10 in 'AoC2017\Task10\uTask_2017_10.pas',
+  uTask_2017_11 in 'AoC2017\Task11\uTask_2017_11.pas',
   uTask_2018_01 in 'AoC2018\Task01\uTask_2018_01.pas',
   uTask_2018_02 in 'AoC2018\Task02\uTask_2018_02.pas',
   uTask_2018_03 in 'AoC2018\Task03\uTask_2018_03.pas',
